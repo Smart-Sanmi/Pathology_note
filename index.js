59ea9efe784e2f7aaacd4c32f1a13109b1d4d74f
Sanmi Odeyemi <mwlgod@gmail.com>
2024-10-21 15:58:07 +0100

    pathology note clone

---
+++ index.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  function openNavL() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
